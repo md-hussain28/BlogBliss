5eca85a2d074dca01849185eb40d13ee8dc4eb65
md-hussain28 <mdsaquib28official@gmail.com>
2024-08-08 18:15:07 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  //console.log(import.meta.env.VITE_WRITE_URL);
+  
+  return (
+    <div className='w-full h-screen bg-black'>
+         <h1 className='text-fuchsia-500'>This is a Blog App</h1>
+    </div>
+  )
+}
+
+export default App
