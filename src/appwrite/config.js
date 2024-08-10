c9f77a34c993735b37e3841b3834848c29d779cc
md-hussain28 <mdsaquib28official@gmail.com>
2024-08-10 21:25:21 +0530

    Auth Added

---
+++ src/appwrite/config.js
@@ -0,0 +1,19 @@
+import conf from "../conf/conf.js";
+import { Client, ID,Databases,Storage,Query} from "appwrite";
+
+export class Service{
+    client =new Client();
+    databases ;
+    bucket;
+
+    constructor(){
+        this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId)
+        this.databases=new Databases(this.client)
+        this.bucket=new Storage(this.client)
+    }
+}
+const service=new Service();
+
+export default service;
